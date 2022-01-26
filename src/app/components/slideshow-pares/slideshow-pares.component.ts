13e036f9a45fb82d49c27fce54a26252bb69b7eb
Daniele <trebolman@gmail.com>
2022-01-26 11:17:53 -0500

    Revert "Revert "Mostrar poster (grandes)""
    This reverts commit f15ad5e8a14a0ddb47ba27d9a4b669f58a2d86e1.

--- src/app/components/slideshow-pares/slideshow-pares.component.ts
+++ src/app/components/slideshow-pares/slideshow-pares.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Pelicula } from 'src/app/interfaces/interfaces';
 
 @Component({
@@ -8,6 +8,7 @@ import { Pelicula } from 'src/app/interfaces/interfaces';
 })
 export class SlideshowParesComponent implements OnInit {
   @Input() peliculas: Pelicula[] = [];
+  @Output() cargaPeliculas =  new EventEmitter();
 
   slideOpts = {
     slidesPerView: 1.3,
@@ -18,4 +19,7 @@ export class SlideshowParesComponent implements OnInit {
 
   ngOnInit() {}
 
+  cargarMas(){
+    this.cargaPeliculas.emit();
+  }
 }
